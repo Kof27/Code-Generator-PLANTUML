2afe527da2a4701882d8c49ea9ce74ccbe202385
markus <markussantiago627@gmail.com>
2024-10-28 17:38:35 -0500

    The GUI is created, still WIP and lacks a lof of functionality

---
+++ src/main/java/com/uao/plantumlcodegenerator/PlantUMLCodeGenerator.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.uao.plantumlcodegenerator;
+import com.uao.plantumlcodegenerator.GUI.*;
+
+/**
+ *
+ * @author santiago
+ */
+public class PlantUMLCodeGenerator {
+
+    public static void main(String[] args) {
+        mainFrame GUI = new mainFrame();
+        GUI.setVisible(true);
+        
+    }
+}
